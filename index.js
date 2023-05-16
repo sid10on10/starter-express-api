94a7ae1ec54fdf04b47d7d1aa25285df8b1700d4
Siddhant Singh <siddhantk.singh.phe14@itbhu.ac.in>
2023-05-16 16:49:34 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+const express = require('express')
+const request = require('request')
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+app.get('/cors', (req, res) => {
+  request(
+    { url: 'https://www.essentiallysports.com/feed/' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message:
+      err.message })
+    }
+    
+    res.set('Content-Type', 'application/rss+xml')
+    res.send(Buffer.from(body))
+  })
 })
 app.listen(process.env.PORT || 3000)
